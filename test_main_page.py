3d2b13b46f4067dfe0f23f4ba3c224b657cfebd5
mo.atamanov <mo.atamanov@NB-201160>
2022-04-04 13:29:25 +0300

    Добавлен файл, содержащий класс главной страницы сайта. Рефакторинг: Инициализация Page Object, добавлены методы открытия страницы и перехода на страницу логина.

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,16 @@
+from pages.main_page import MainPage
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    # инициализация Page Object, передача в конструктор экземпляра драйвера и url адреса
+    page = MainPage(browser, link)
+    # открывает страницу
+    page.open()
+    # выполняет метод страницы — переходит на страницу логина
+    page.go_to_login_page()
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
