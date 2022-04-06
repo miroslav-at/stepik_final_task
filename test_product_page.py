761d4fe32e8398579e7301512a36d99877243193
mo.atamanov <mo.atamanov@NB-201160>
2022-04-06 14:23:32 +0300

    Реализована параметризация теста, помечен падающий тест.

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,18 @@
+import pytest
 from pages.product_page import ProductPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+xfile = 7
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+links = [f"{product_base_link}/?promo=offer{number}" for number in range(10) if number != xfile]
+# Страница с багом
+xlink = pytest.param(f"{product_base_link}/?promo=offer{xfile}", marks=pytest.mark.xfail(reason="mistake on page"))
+# Вставляет элемент перед указанным индексом
+links.insert(xfile, xlink)
 
 
+@pytest.mark.parametrize('link', links)
 # Проверяет добавление в корзину со страницы товара
-def test_guest_can_add_product_to_basket(browser):
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
